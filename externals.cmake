ea9ec808306c5ebb9f53b5de2a2c104bbe434fb6
Mathew Benson <mathew@benson.co.ke>
2024-10-22 03:12:43 +0300

    add git tag for googletest

--- externals.cmake
+++ externals.cmake
@@ -95,6 +95,7 @@ FetchContent_Declare(
 Fetchcontent_Declare(
     GTest
     GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.15.2
     GIT_SHALLOW TRUE
     FIND_PACKAGE_ARGS
 )
